02e9c9c199537a2a04c868a93876b4631ed74cbf
Anthony-coco <2396821879@qq.com>
2020-10-15 21:21:45 +0800

    10/6 ann app

--- app/src/main/java/com/fed/MainActivity.java
+++ app/src/main/java/com/fed/MainActivity.java
@@ -6,27 +6,22 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.fedclient.R;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
-        EditText userName=findViewById(R.id.userName);
-        EditText Password=findViewById(R.id.passwd);
+        EditText user_name = findViewById(R.id.user_name);
+        EditText password = findViewById(R.id.password);
         Button btn_register = findViewById(R.id.btn_register);
         Button btn_login=findViewById(R.id.btnLogin); //获取xml中的元素
-
-        String username = getIntent().getStringExtra("username");//从注册界面传过来的数据
-        String password = getIntent().getStringExtra("psw");
-        userName.setText(username);
-        Password.setText(password);
 
         //注册控件的点击事件
         btn_register.setOnClickListener( new View.OnClickListener() {
@@ -42,9 +37,19 @@ public class MainActivity extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String Username = user_name.getText().toString().trim();
+                String Password =password.getText().toString().trim();
 
-                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(intent);
+                String init_name = "name";
+                String init_passwd = "123456";
+
+                if(Username.equals(init_name) && Password.equals(init_passwd)){
+                    Toast.makeText(MainActivity.this,"登陆成功", Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this,"密码错误", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
--- app/src/main/java/com/fed/NowActivity.java
+++ app/src/main/java/com/fed/NowActivity.java
@@ -1,25 +1,61 @@
 package com.fed;
 
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import com.fedclient.R;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class NowActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class NowActivity<TimeLineView, DiscreteSeekBar> extends AppCompatActivity {
     private RadioButton RB_renwuguanli;
     private RadioButton RB_lishi;
     private RadioButton RB_now;
     private RadioButton RB_shezhi;
     private RadioButton RB_mine;
 
+    private ProgressBar progesss;
+    private TextView progesssValue;
+    private LinearLayout full;
+    private Object ToastUtil;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_now);
+
+        progesss = (ProgressBar) findViewById(R.id.progesss1);
+        progesssValue = (TextView) findViewById(R.id.progesss_value1);
+        full = (LinearLayout) findViewById(R.id.full);
+
+        initview();
 
         RB_renwuguanli = (RadioButton) findViewById (R.id.RB_renwuguanli);
         RB_renwuguanli.setOnClickListener(new View.OnClickListener() {
@@ -61,5 +97,48 @@ public class NowActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void initview() {
+        progesss.setProgress(66);
+        progesssValue.setText(new StringBuffer().append(progesss.getProgress()).append("%"));
+        setPosWay1();
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            setPos();
+        }
+    }
+    private void setPosWay1() {
+        progesssValue.post(new Runnable() {
+            @Override
+            public void run() {
+                setPos();
+            }
+        });
+    }
+
+    /**
+     * 设置进度显示在对应的位置
+     */
+    public void setPos() {
+        int w = getWindowManager().getDefaultDisplay().getWidth();
+        Log.e("w=====", "" + w);
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) progesssValue.getLayoutParams();
+        int pro = progesss.getProgress();
+        int tW = progesssValue.getWidth();
+        if (w * pro / 100 + tW * 0.3 > w) {
+            params.leftMargin = (int) (w - tW * 1.1);
+        } else if (w * pro / 100 < tW * 0.7) {
+            params.leftMargin = 0;
+        } else {
+            params.leftMargin = (int) (w * pro / 100 - tW * 0.7);
+        }
+        progesssValue.setLayoutParams(params);
+
     }
 }
