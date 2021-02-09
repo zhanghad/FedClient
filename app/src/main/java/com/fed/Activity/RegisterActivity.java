88ccff3c7a01c4f920868f78e04b8bf99caafc7d
Anthony-coco <2396821879@qq.com>
2021-02-09 18:50:48 +0800

    10/6 ann app

--- app/src/main/java/com/fed/Activity/RegisterActivity.java
+++ app/src/main/java/com/fed/Activity/RegisterActivity.java
@@ -11,97 +11,112 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.fedclient.R;
+import com.fed.util.Util;
+import com.fed.util.CommonRequest;
+import com.fed.util.CommonResponse;
+import com.fed.util.HttpUtil;
+import com.fed.util.Consts;
+
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Response;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button btn_register;//注册按钮
     private Button to_login; //返回按钮
-    private EditText et_user_name, et_psw1, et_psw2;
-    private String user_name, psw1, psw2;
+    private EditText et_Username, et_pwd1, et_pwd2;
+    private String user_name, pwd1, pwd2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedIdnstanceState) {
+        super.onCreate(savedIdnstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_register);
-        init();
+        initComponents();
+        setListeners();
     }
 
-    private void init() {
-        btn_register=findViewById(R.id.btn_register);
-        to_login=findViewById(R.id.to_login);
+    void initComponents() {
+        btn_register = findViewById(R.id.btn_register);
+        to_login = findViewById(R.id.to_login);
+        et_Username = findViewById(R.id.et_username);
+        et_pwd1 = findViewById(R.id.et_pwd1);
+        et_pwd2 = findViewById(R.id.et_pwd2);
+    }
 
-        et_user_name=findViewById(R.id.et_username);
-        et_psw1=findViewById(R.id.et_psw1);
-        et_psw2=findViewById(R.id.et_psw2);
-
+    void setListeners(){
         //返回
         to_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //为了跳转到登录界面
-                Intent intent=new Intent(RegisterActivity.this,MainActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                finish();
             }
         });
-
         //注册按钮
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                getEditString();
-                if(!psw1.equals(psw2)){
-                    Toast.makeText(RegisterActivity.this, "输入两次的密码不一样", Toast.LENGTH_SHORT).show();
-                    return;
-                }else if(isExistUserName(user_name)){
-                    Toast.makeText(RegisterActivity.this, "此账户名已经存在", Toast.LENGTH_SHORT).show();
-                    return;
-                }else{
-                    Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
-                    saveRegisterInfo(user_name, psw1);
-                    //注册成功后把账号传递到LoginActivity.java中
-                    // 返回值到loginActivity显示
-                    Intent data = new Intent();
-                    data.putExtra("userName", user_name);
-                    setResult(RESULT_OK, data);
-                    //RESULT_OK为Activity系统常量，状态码为-1，
-                    // 表示此页面下的内容操作成功将data返回到上一页面，如果是用back返回过去的则不存在用setResult传递data值
-                    RegisterActivity.this.finish();
-                }
+            public void onClick(View view) {
+                register();
             }
         });
     }
 
-    private void getEditString() {
-        user_name=et_user_name.getText().toString().trim();
-        psw1=et_psw1.getText().toString().trim();
-        psw2=et_psw2.getText().toString().trim();
-    }
+    private void register(){
+        CommonRequest request = new CommonRequest();
 
-    private void saveRegisterInfo(String user_name, String psw1) {
-        String md5Psw = MD5Utils.md5(psw1);//把密码用MD5加密
-        //loginInfo表示文件名, mode_private SharedPreferences sp = getSharedPreferences( );
-        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        //获取编辑器， SharedPreferences.Editor  editor -> sp.edit();
-        SharedPreferences.Editor editor=sp.edit();
-        //以用户名为key，密码为value保存在SharedPreferences中
-        //key,value,如键值对，editor.putString(用户名，密码）;
-        editor.putString(user_name, md5Psw);
-        //提交修改 editor.commit();
-        editor.commit();
-    }
+        String Username = Util.StringHandle(et_Username.getText().toString());
+        String pwd = Util.StringHandle(et_pwd1.getText().toString());
+        String pwd_confirm = Util.StringHandle(et_pwd2.getText().toString());
 
-    private boolean isExistUserName(String user_name) {
-        boolean has_userName=false;
-        //mode_private SharedPreferences sp = getSharedPreferences( );
-        // "loginInfo", MODE_PRIVATE
-        SharedPreferences sp=getSharedPreferences("loginInfo", MODE_PRIVATE);
-        //获取密码
-        String spPsw=sp.getString(user_name, "");//传入用户名获取密码
-        //如果密码不为空则确实保存过这个用户名
-        if(!TextUtils.isEmpty(spPsw)) {
-            has_userName=true;
+        String resMsg = checkDataValid(Username,pwd,pwd_confirm);
+        if(!resMsg.equals("")){
+            showResponse(resMsg);
+            return;
         }
-        return has_userName;
+
+        request.addRequestParam("Username",Username);
+        request.addRequestParam("pwd",pwd);
+
+        HttpUtil.sendPost(Consts.URL_Register, request.getJsonStr(), new okhttp3.Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                CommonResponse res = new CommonResponse(response.body().string());
+                String resCode = res.getResCode();
+                String resMsg = res.getResMsg();
+                // 显示注册结果
+                showResponse(resMsg);
+                // 注册成功
+                if (resCode.equals(Consts.SUCCESSCODE_REGISTER)) {
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                showResponse("Network ERROR");
+            }
+        });
+    }
+
+    private void showResponse(final String msg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(RegisterActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private String checkDataValid(String Username,String pwd,String pwd_confirm){
+        if(TextUtils.isEmpty(Username) | TextUtils.isEmpty(pwd) | TextUtils.isEmpty(pwd_confirm))
+            return getResources().getString(R.string.null_hint);
+        if(!pwd.equals(pwd_confirm))
+            return getResources().getString(R.string.not_equal_hint);
+        if(Username.length() != 11 && !Username.contains("@"))
+            return getResources().getString(R.string.account_invalid_hint);
+        return "";
     }
 }
